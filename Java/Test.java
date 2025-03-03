2228a46b78888a67869a6b93b4fd88236d79771e
ArunK <arunkarthic369@gmail.com>
2025-03-03 15:20:23 +0530

    Initial agn

---
+++ Java/Test.java
@@ -0,0 +1,56 @@
+// import java.util.Scanner;
+
+// class Test{
+//     static int a,b,s=0;
+//     static void Get(){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the first no: ");
+//         a = sc.nextInt();
+//         System.out.print("Enter the second no: ");
+//         b = sc.nextnt();
+//     }
+//     static void Cal(){
+//         s = a+b;
+//     }
+//     static void Dis(){
+//         System.out.println(s);
+//     }
+//     public static void main(String[] args){
+//         Get();
+//         Cal();
+//         Dis();
+//     }
+// }
+
+import java.util.Scanner;
+
+class Perfect {
+    static int n, s = 0, i;
+
+    static void Get(int x) {
+        n = x;
+        Cal();
+    }
+
+    static void Cal() {
+        for (i = 1; i < n; i++) {
+            if (n % i == 0) {
+                s += i;
+            }
+        }
+        Dis();
+    }
+
+    static void Dis() {
+        System.out.println(n == s ? "Yup" : "Nope");
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no: ");
+        int n = sc.nextInt();
+        Perfect.Get(n);
+    }
+}
