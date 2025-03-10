f8d3779857f80e55c4b143ba26c82e37f249fc44
ArunK <arunkarthic369@gmail.com>
2025-03-10 14:50:16 +0530

    Strings cp 2

--- Basics-of-strings/Example.java
+++ Basics-of-strings/Example.java
@@ -60,16 +60,44 @@
 //     }
 // }
 
+// class Example{
+//     public static void main(String[] args) {
+//         String s1 = "Java is boring";
+//         System.out.println(s1.startsWith("Java"));
+//         System.err.println(s1.startsWith("J", 0));
+//         System.out.println(s1.startsWith("Java", 0));
+//         System.out.println(s1.startsWith("a", 0));
+//         System.out.println(s1.startsWith("is", 5)); 
+//         System.out.println(s1.endsWith("g"));
+
+//     }
+// }
+
+// class Example{
+//     public static void main(String[] args) {
+//         String s1 = "Java is boring";
+//         int index = s1.indexOf("is");
+//         System.out.println(index);
+//         int index2 = s1.indexOf("is", 1);
+//         System.out.println(index2);
+//         int index3 = s1.lastIndexOf("is");
+//         System.out.println(index3);       
+//     }
+// }
+
 class Example{
     public static void main(String[] args) {
-        String s1 = "Java is boring";
-        System.out.println(s1.startsWith("Java"));
-        System.err.println(s1.startsWith("J", 0));
-        System.out.println(s1.startsWith("Java", 0));
-        System.out.println(s1.startsWith("a", 0));
-        System.out.println(s1.startsWith("is", 5)); 
-        System.out.println(s1.endsWith("g"));
 
+        String s1 = "Java is boring";
+        // char ch = sc.next().charAt(0);
+        // System.out.println(ch);
+        System.out.println(s1.indexOf("J"));
+        System.out.println(s1.indexOf("is"));
+        System.out.println(s1.indexOf("is", 5));
+        System.out.println(s1.lastIndexOf("is"));
+        System.out.println(s1.lastIndexOf("a", 2)); // 1 from 2 a last is at 1.
+        System.out.println(s1.lastIndexOf("a", 5)); // 3 from 5 a last is at 3.
+        System.out.println(s1.lastIndexOf("g", 13)); // 1 so basically lastIndexOf is also finding the first occurence of the character from the end of the string (in reverse). 
+        System.out.println(s1.lastIndexOf("g", 0)); // -1 so here from 0 there is no g, but from 13 g is at 13.
     }
 }
-
